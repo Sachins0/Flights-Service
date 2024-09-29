b707a9fbb0d934c8cab2b3a60c6c6a8f2c89109a
Sachins0 <sachins.ssingh0@gmail.com>
2024-09-29 14:08:17 +0530

    basic CRUD functionality

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 
 
 module.exports={
-    infoController: require('./info-controller')
+    infoController: require('./info-controller'),
+    AirplaneController: require('./airplane-controller')
 }
